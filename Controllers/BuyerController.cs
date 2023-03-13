94a1af139f48762f2aeb158d943daacbd4992dfc
akashlad22 <akashsky24.al@gmail.com>
2023-03-13 13:53:40 +0530

    inital commit

---
+++ Controllers/BuyerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AgroEcommerce.Controllers
+{
+    public class BuyerController : Controller
+    {
+        // GET: Buyer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult My_Account()
+        {
+            return View(); 
+        }
+
+
+
+    }
+}
---
+++ Scripts/Buyer_js/LanguageJavaScript.js
@@ -0,0 +1,36 @@
+﻿
+   
+        var arrLang = {
+            'ma': {
+                'My Account': 'माझे खाते',
+                'Our location': 'आमचे स्थान',
+                'Contact Us': 'आमच्याशी संपर्क साधा',
+                'SHOP': 'दुकान',
+                ' About Us': 'आमच्याबद्दल',
+                'Gallery':'गॅलरी',
+                'Home': 'मुख्यपृष्ठ',
+                'ABOUT US': 'आमच्याबद्दल',
+            },
+            'hi': {
+                'My Account': 'मेरा खाता',
+                'Our location': 'हमारा स्थान ',
+                'Contact Us': 'संपर्क करें',
+                'SHOP': 'दुकान',
+                ' About Us ': 'हमारे बारे में',
+                'Gallery': 'गेलरी ',
+                'Home': 'घर',
+                'ABOUT US': 'हमारे बारे में',
+            }
+        };
+        $(function () {
+
+            $('.translate').click(function () {
+                var lang = $(this).attr('id');
+
+                $('.lang').each(function (index, element) {
+                    $(this).text(arrLang[lang][$(this).attr('key')]);
+
+                });
+            });
+        });
+  
